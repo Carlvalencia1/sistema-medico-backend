f45846faa581e99d21040a0a03d85f64e3a76d90
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2025-03-21 07:32:15 -0600

    add: routes

--- src/feautures/cases/infrastructure/medicalCase_routes.go
+++ src/feautures/cases/infrastructure/medicalCase_routes.go
@@ -8,24 +8,33 @@ import (
 type MedicalCaseRoutes struct {
 	engine               *gin.Engine
 	createMedicalCaseController *controllers.CreateMedicalCaseController
+	getAllMedicalCaseController *controllers.GetAllMedicalCaseController
+	updateMedicalCaseController *controllers.UpdateMedicalCaseController
+	deleteMedicalCaseController *controllers.DeleteMedicalCaseController
 }
 
-func NewMedicalCaseRoutes(engine *gin.Engine, createMedicalCaseController *controllers.CreateMedicalCaseController) *MedicalCaseRoutes {
+func NewMedicalCaseRoutes(engine *gin.Engine, createMedicalCaseController *controllers.CreateMedicalCaseController, getAllMedicalCaseController *controllers.GetAllMedicalCaseController, updateMedicalCaseController *controllers.UpdateMedicalCaseController, deleteMedicalCaseController *controllers.DeleteMedicalCaseController ) *MedicalCaseRoutes {
 	return &MedicalCaseRoutes{
 		engine:               engine,
 		createMedicalCaseController: createMedicalCaseController,
+		getAllMedicalCaseController: getAllMedicalCaseController,
+		updateMedicalCaseController: updateMedicalCaseController,
+		deleteMedicalCaseController: deleteMedicalCaseController,
 	}
 }
 
 func (r *MedicalCaseRoutes) SetupRoutes() {
 	medical := r.engine.Group("/medicalcases")
 	{
+		medical.GET("/", r.getAllMedicalCaseController.GetAll)
 		medical.POST("/", r.createMedicalCaseController.Create)
+		medical.PUT("/:id", r.updateMedicalCaseController.UpdateMedicalCase)
+		medical.DELETE("/:id", r.deleteMedicalCaseController.Delete)
 	}
 }
 
 func (r *MedicalCaseRoutes) Run() error {
-	if err := r.engine.Run(":8081"); err != nil {
+	if err := r.engine.Run(":8080"); err != nil {
 		return err
 	}
 	return nil
