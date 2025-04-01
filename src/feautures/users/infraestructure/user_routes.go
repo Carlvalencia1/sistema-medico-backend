78b545c32ce162d0618e976819f6ef330406fb97
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2025-04-01 06:11:22 -0600

    add:  MySQL

--- src/feautures/users/infraestructure/user_routes.go
+++ src/feautures/users/infraestructure/user_routes.go
@@ -1,1 +1,31 @@
 package infraestructure
+
+import (
+	"github.com/gin-gonic/gin"
+	"smartvitals/src/feautures/users/infraestructure/controllers"
+)
+
+	type UserRoutes struct {
+		engine              *gin.Engine
+		createUserController *controllers.CreateUserController
+		loginController   *controllers.LogInController
+		getUsersController  *controllers.GetUsersController 
+		updateUserController *controllers.UpdateUserController
+		deleteUserController *controllers.DeleteUserController
+		getByUsernameController *controllers.GetByUsernameController
+		
+	
+	}
+
+	func NerUserRoutes (engine *gin.Engine, createUserController *controllers.CreateUserController, loginController *controllers.LogInController, getUsersController *controllers.GetUsersController, updateUserController *controllers.UpdateUserController, deleteUserController *controllers.DeleteUserController, getByUsernameController *controllers.GetByUsernameController) *UserRoutes {
+		return &UserRoutes{
+			engine: engine,
+			createUserController: createUserController,
+			loginController: loginController,
+			getUsersController: getUsersController,
+			updateUserController: updateUserController,
+			deleteUserController: deleteUserController,
+			getByUsernameController: getByUsernameController,
+		}
+		
+	}
