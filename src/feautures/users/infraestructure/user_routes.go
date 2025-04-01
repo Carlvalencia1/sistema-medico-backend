15ad5a48004a697ba0a452dae2684c03050d33c7
Carlvalencia1 <233428@ids.upchiapas.edu.mx>
2025-04-01 06:27:17 -0600

    add: Routes usersL

--- src/feautures/users/infraestructure/user_routes.go
+++ src/feautures/users/infraestructure/user_routes.go
@@ -13,11 +13,12 @@ import (
 		updateUserController *controllers.UpdateUserController
 		deleteUserController *controllers.DeleteUserController
 		getByUsernameController *controllers.GetByUsernameController
+		getUserByIdController *controllers.GetUserByIdController
 		
 	
 	}
 
-	func NerUserRoutes (engine *gin.Engine, createUserController *controllers.CreateUserController, loginController *controllers.LogInController, getUsersController *controllers.GetUsersController, updateUserController *controllers.UpdateUserController, deleteUserController *controllers.DeleteUserController, getByUsernameController *controllers.GetByUsernameController) *UserRoutes {
+	func NerUserRoutes (engine *gin.Engine, createUserController *controllers.CreateUserController, loginController *controllers.LogInController, getUsersController *controllers.GetUsersController, updateUserController *controllers.UpdateUserController, deleteUserController *controllers.DeleteUserController, getByUsernameController *controllers.GetByUsernameController, getUserByIdController *controllers.GetUserByIdController) *UserRoutes {
 		return &UserRoutes{
 			engine: engine,
 			createUserController: createUserController,
@@ -26,6 +27,26 @@ import (
 			updateUserController: updateUserController,
 			deleteUserController: deleteUserController,
 			getByUsernameController: getByUsernameController,
+			getUserByIdController: getUserByIdController,
 		}
 		
 	}
+
+	func (r *UserRoutes) SetupRoutes() {
+		UserRoutes := r.engine.Group("/users")
+		{
+			UserRoutes.POST("/", r.createUserController.Run)
+			UserRoutes.POST("/login", r.loginController.Run)
+			UserRoutes.GET("/", r.getUsersController.Run)
+			UserRoutes.GET("username/:username", r.getByUsernameController.Run)
+			UserRoutes.PUT("/:id", r.updateUserController.Run)
+			UserRoutes.DELETE("/:id", r.deleteUserController.Run)
+			UserRoutes.GET("/:id", r.getUserByIdController.Run)
+		}
+	}
+	func (r *UserRoutes) Run() error {
+		if err := r.engine.Run(); err != nil {
+            return err
+        }
+        return nil
+    }
